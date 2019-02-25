71f325337e58ae6bfe93d4bf0bebc65d67c0c57c
Damien Moyer <dmoyer@stu.jsu.edu>
2019-02-25 11:28:17 -0600

    Working on connection

--- src/TASDatabase.java
+++ src/TASDatabase.java
@@ -8,6 +8,58 @@
  *
  * @author Dustin Moody
  */
+import java.sql.*;
+import org.json.simple.*;
+
 public class TASDatabase {
+
+    public static void main(String[] args){
+        JSONArray results = getJSONData();
+    }
     
+    public static JSONArray getJSONData(){
+    
+        JSONArray results = new JSONArray();
+    
+        Connection conn = null;
+        PreparedStatement pstSelect = null, pstUpdate = null; 
+        ResultSet resultset = null;
+        ResultSetMetaData metadata = null;
+
+        String query;
+
+        boolean hasresults;
+        int resultCount, columnCount, updateCount = 0;
+
+        try {
+
+            /* Identify the Server */
+
+            String server = ("jbdc:mysql://localhost/tas");
+            String username = "teamc";
+            String password = "CS310";
+            System.out.println("Connecting to " + server + "...");
+            
+            /* Load the MySQL JDBS Drive */
+        
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        
+            /* Open Connection */
+        
+            conn = DriverManager.getConnection(server, username, password);
+            
+            /* Test Connection */
+            
+            if (conn.isValid(0)){
+                
+                /* Connection Open! */
+                
+                System.out.println("Connected Succesfully!"); 
+            }
+        }
+        
+        
+        
+        return results; 
+    }
 }

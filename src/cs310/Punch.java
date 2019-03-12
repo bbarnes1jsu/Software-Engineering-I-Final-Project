1ac05699efa973970de766a042ed9a626794b986
Brandon Barnes <bbarnes1.stu.jsu.edu>
2019-03-12 11:08:43 -0500

    Did some cleanup and started feature 3/add test files

--- src/cs310/Punch.java
+++ src/cs310/Punch.java
@@ -9,14 +9,13 @@ import java.time.format.DateTimeFormatter;
  */
 public class Punch {
     
-    
     GregorianCalendar cal = new GregorianCalendar();
+    GregorianCalendar cal2 = new GregorianCalendar();
     
     private int terminalid;
     private int punchtypeid;
-    //private int id;
-    private long timeStamp;
-    private long adjustedTimeStamp;
+    private long timeStamp = 0;
+    private long adjustedTimeStamp = 0;
     private String badgeId;
     
     //Constructor
@@ -24,12 +23,12 @@ public class Punch {
         
         this.badgeId = badge.getID();
         this.punchtypeid = punchtypeid;
-        //this.id = id;
         this.terminalid = terminalid;        
     }
     
     //This method will print the original time stamp based on what type of punch employee makes. 
     public String printOriginalTimestamp(){
+        
         String punch = null;
         cal = new GregorianCalendar();
         cal.setTimeInMillis(timeStamp);
@@ -52,8 +51,53 @@ public class Punch {
     
     public void adjust(Shift s){
         
+        GregorianCalendar shiftStart = new GregorianCalendar();
+        GregorianCalendar shiftStop = new GregorianCalendar();
+        GregorianCalendar lunchStart = new GregorianCalendar();
+        GregorianCalendar lunchStop = new GregorianCalendar();
+        GregorianCalendar startInterval = new GregorianCalendar();
+        GregorianCalendar stopInterval = new GregorianCalendar();
+        GregorianCalendar startGrace = new GregorianCalendar();
+        GregorianCalendar stopGrace = new GregorianCalendar();
+        GregorianCalendar startDock= new GregorianCalendar();
+        GregorianCalendar stopDock = new GregorianCalendar();
+                   
+        cal = new GregorianCalendar();
+        cal.setTimeInMillis(timeStamp);
+        long originalTimeStampInMillis = cal.getTimeInMillis();
+        cal2.setTimeInMillis(originalTimeStampInMillis);
+        
+        shiftStart.setTimeInMillis(originalTimeStampInMillis);
+        shiftStart.set(Calendar.HOUR_OF_DAY, s.getStart().getHours());
+        shiftStart.set(Calendar.MINUTE, s.getStart().getMinutes());
+        shiftStart.set(Calendar.SECOND, 0);
+        long shiftStartInMillis = shiftStart.getTimeInMillis();
+
+        startDock.setTimeInMillis(shiftStartInMillis);
+        
+        startInterval.setTimeInMillis(shiftStartInMillis);
+        
+        startGrace.setTimeInMillis(shiftStartInMillis);
     }
-    
+    public String printAdjustedTimestamp(){
+        
+        String punch = null;
+        
+        switch (punchtypeid) {
+            case 0:
+                punch = "#" + badgeId + " CLOCKED OUT: " + cal.toZonedDateTime().format(DateTimeFormatter.ofPattern("E MM/dd/uuuu HH:mm:ss"));
+                break;
+            case 1:
+                punch = "#" + badgeId + " CLOCKED IN: " + cal.toZonedDateTime().format(DateTimeFormatter.ofPattern("E MM/dd/uuuu HH:mm:ss"));
+                break;
+            case 2:
+                punch = "#" + badgeId + " TIMED OUT: " + cal.toZonedDateTime().format(DateTimeFormatter.ofPattern("E MM/dd/uuuu HH:mm:ss"));
+                break;
+            default:
+                break;
+        }
+        return punch.toUpperCase();
+    }
     public String getBadgeId(){
         return badgeId;
     }

d7d2263533025be35522fafe42c5e896dc7caccd
Damien Moyer <dmoyer@stu.jsu.edu>
2019-03-15 11:37:27 -0500

    Implemented Dexter's insertPunch

--- src/cs310/TASDatabase.java
+++ src/cs310/TASDatabase.java
@@ -428,9 +428,62 @@ public class TASDatabase{
        
    }
    
-   public int insertPunch(Punch p){
-       return 0; //Get rid of this later
-   }
+    public int insertPunch(Punch p){
+        PreparedStatement pstSelect = null, pstUpdate = null;
+        ResultSet resultset = null;
+        String query;
+        int updateCount = 0;
+        Punch punch = p;
+
+        int id = 0;
+        Long timeStamp = punch.getOriginalTimeStamp();
+        int punchTypeId = punch.getPunchTypeId();
+        String badgeId = punch.getBadgeId();
+        int terminalid = punch.getTerminalId();
+
+        try{
+            
+            if (conn.isValid(0)){
+                Statement stmt = conn.createStatement( );
+
+                query = "INSERT INTO punch ( badgeid, terminalid, originaltimestamp, punchtypeid) VALUES (?, ?, ?, ?)";
+                pstUpdate = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+                pstUpdate.setString(1,badgeId);
+                pstUpdate.setInt(2, terminalid);                        
+                pstUpdate.setLong(3,timeStamp);
+                pstUpdate.setInt(4,punchTypeId);
+
+               
+                updateCount = pstUpdate.executeUpdate();
+                if (updateCount > 0) {
+                    resultset = pstUpdate.getGeneratedKeys();
+                }
+                
+                ResultSet result = stmt.executeQuery("SELECT * FROM punch ORDER BY id DESC limit 1;");
+                
+                if (result != null){
+                    result.next();
+                    id = result.getInt("id");
+                }
+            }
+        }
+
+        catch (Exception e){
+        System.err.println(e.toString());
+        }
+
+        finally {
+
+        if (resultset != null) { try { resultset.close(); resultset = null; } catch (Exception e) {} }
+
+        if (pstSelect != null) { try { pstSelect.close(); pstSelect = null; } catch (Exception e) {} }
+
+        if (pstUpdate != null) { try { pstUpdate.close(); pstUpdate = null; } catch (Exception e) {} }
+
+        }
+        return id;
+        }
    
    public ArrayList<Punch> getDailyPunchList(Badge b, Long ts){
        
